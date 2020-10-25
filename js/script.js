cb32fe05d1fa43d6d3ce431fd8efb7805184f6fc
Ushakov Artur <ushakov.artur93@gmail.com>
2020-10-25 21:31:51 +0500

    Добавил функцию управления и десятичных значений

--- js/script.js
+++ js/script.js
@@ -16,4 +16,34 @@ function inputValue(value) {
   }
 }
 
+function inputDecimal(dot) {
+
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
+    
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator
+    const inputValue = parseFloat(displayValue);
+    
+    if (operator && calculator.waitingForSecondOperand)  {
+        calculator.operator = nextOperator;
+        return;
+    }
+    
+    if (firstOperand == null) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const currentValue = firstOperand || 0;
+        const result = performCalculation[operator](currentValue, inputValue);
+    
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+    
+      calculator.waitingForSecondOperand = true;
+      calculator.operator = nextOperator;
+}
+
 inputValue(value);
